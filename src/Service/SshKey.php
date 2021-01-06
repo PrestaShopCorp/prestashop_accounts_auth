2f0d83b9c188a1fd630efc28d5aa17b9c4095f33
Hervé Schoenenberger <herve.schoenenberger@prestashop.com>
2021-01-06 11:42:23 +0100

    cs-fixer

--- src/Service/SshKey.php
+++ src/Service/SshKey.php
@@ -74,6 +74,6 @@ class SshKey
     {
         $this->rsa->loadKey($publicKey, RSA::PUBLIC_FORMAT_PKCS1);
 
-        return  $this->rsa->verify($data, base64_decode($signature));
+        return $this->rsa->verify($data, base64_decode($signature));
     }
 }
